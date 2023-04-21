ca9bb08119ed5fc6de046b64e778c9a6d58a818b
vj4173177 <svijaykweb@gmail.com>
2023-04-21 16:24:12 +0530

    first commit

---
+++ app/src/main/java/com/example/mvvm_arch/HomeScreen.kt
@@ -0,0 +1,21 @@
+package com.example.mvvm_arch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class HomeScreen : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    }
+
+}
